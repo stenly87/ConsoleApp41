3b9e1967d83956630c5703c871da420844f41840
Alex Pushkin <ifc@suz-ppk.ru>
2023-12-04 13:29:13 +1000

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace ConsoleApp41
+{
+    class Program
+    { 
+        public static void Main(string[] args)
+        {
+            //Паттерн Синглтон (Singleton)
+            //порождающий паттерн
+            //гарантирует, что для заданного класса
+            //на все приложение будет существовать только
+            //один экземпляр
+            // например это ссылка на базу данных
+
+            // получение объекта через синглтон
+            PPK ppk = PPK.GetInstance();
+            // далее используем объект как обычно
+            Console.WriteLine(ppk.Name);
+
+            // пример использования
+            List<Student> studs = StudentBase.
+                GetInstance().GetStudents("Паша");
+
+            // Строитель (Builder)
+            // порождающий паттерн
+            // Применяется в ситуациях, когда создаваемый
+            // объект настраивается вариативным образом
+            // билдер предоставляет методы для добавления
+            // вариаций и создает конечный объект
+
+
+            // пример использования билдера
+            BurgerBuilder burgerBuilder = new BurgerBuilder();
+            Cooker cooker = new Cooker(burgerBuilder);
+            var вкусныйСочныйБургерМечты = cooker.CookBurger();
+            Console.WriteLine(вкусныйСочныйБургерМечты);
+            BadCooker badCooker = new BadCooker(burgerBuilder);
+            var НЕвкусныйСочныйБургерМечты = badCooker.CookBurger();
+            Console.WriteLine(НЕвкусныйСочныйБургерМечты);
+        }
+    }
+
+    public class PPK
+    {
+        // начало синглтон (простая реализация, без учета
+        // потоков
+        static PPK instance;
+
+        private PPK() {
+            Name = "КГА ПОУ ППК"; // пример
+        }
+
+        static object locker = new object(); // эта строка добавляется при реализации с учетом потоков
+        public static PPK GetInstance()
+        {
+            lock (locker) // эта строка добавляется при реализации с учетом потоков
+            {
+                if (instance == null)
+                    instance = new PPK();
+                return instance;
+            }
+        }
+        // конец синглтон
+
+        // далее обычный класс с данными и функциями
+        public string Name { get; private set; }
+    }
+
+    public class StudentBase
+    {
+        static StudentBase instance;
+
+        private StudentBase()
+        {
+            // загрузка из файла List<Student>();
+        }
+
+        static object locker = new object(); // эта строка добавляется при реализации с учетом потоков
+        public static StudentBase GetInstance()
+        {
+            lock (locker) // эта строка добавляется при реализации с учетом потоков
+            {
+                if (instance == null)
+                    instance = new StudentBase();
+                return instance;
+            }
+        }
+
+        List<Student> students = new List<Student>();
+        public List<Student> GetStudents(string search)
+        {
+            //  фильтрация
+            return students;
+        }
+
+        public void AddStudent(Student student)
+        {
+            students.Add(student);
+        }
+
+        // и тп
+    }
+
+    public class Student
+    {
+    }
+}
---
+++ builderSample.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp41
+{
+    public class Burger
+    {
+        public Component BaseComponent { get; set; }
+        public Component SecondComponent { get; set; }
+        public Component ThirdComponent { get; set; }
+        public Component FourthComponent { get; set; }
+
+        public override string ToString()
+        {
+            return $"Бургер. Состав: {BaseComponent} {SecondComponent} {ThirdComponent} {FourthComponent} ";
+        }
+    }
+
+    public abstract class Component
+    {
+    }
+
+    public class Булочка : Component
+    {
+    }
+
+    public class ЧерствыйХлеб : Component
+    {
+    }
+
+    public class Соус : Component
+    {
+    }
+
+    public class Паучок : Component
+    {
+    }
+
+    public class Котлета : Component
+    {
+    }
+
+    public class КотлетаКошка : Component
+    {
+    }
+
+    public class Кипченез : Component
+    {
+    }
+
+    public class BurgerBuilder
+    {
+        Burger burger;
+
+        public BurgerBuilder()
+        {
+            burger = new Burger();
+        }
+
+        public BurgerBuilder AddBread()
+        {
+            burger.BaseComponent = new Булочка();
+            return this;
+        }
+
+        public BurgerBuilder AddBadBread()
+        {
+            burger.BaseComponent = new ЧерствыйХлеб();
+            return this;
+        }
+
+        public BurgerBuilder AddSauce()
+        {
+            burger.ThirdComponent = new Соус();
+            return this;
+        }
+
+        public BurgerBuilder AddBadSauce()
+        {
+            burger.ThirdComponent = new Кипченез();
+            return this;
+        }
+
+        public BurgerBuilder AddCutlet()
+        {
+            burger.SecondComponent = new Котлета();
+            return this;
+        }
+
+        public BurgerBuilder AddBadCutlet()
+        {
+            burger.SecondComponent = new КотлетаКошка();
+            return this;
+        }
+
+        public BurgerBuilder AddSecredIngredient()
+        {
+            burger.FourthComponent = new Паучок();
+            return this;
+        }
+
+        public Burger GetResult()
+        {
+            return burger;
+        }
+
+        public BurgerBuilder Clear()
+        {
+            burger = new Burger();
+            return this;
+        }
+    }
+
+    public class Cooker
+    {
+        private readonly BurgerBuilder burgerBuilder;
+
+        public Cooker(BurgerBuilder burgerBuilder)
+        {
+            this.burgerBuilder = burgerBuilder;
+        }
+
+        public Burger CookBurger()
+        {
+            return burgerBuilder
+                .Clear()
+                .AddBread()
+                .AddCutlet()
+                .AddSauce()
+                .GetResult();
+        }
+    }
+
+    public class BadCooker
+    {
+        private readonly BurgerBuilder burgerBuilder;
+
+        public BadCooker(BurgerBuilder burgerBuilder)
+        {
+            this.burgerBuilder = burgerBuilder;
+        }
+
+        public Burger CookBurger()
+        {
+            return burgerBuilder
+                .AddBadBread()
+                .AddBadCutlet()
+                .AddBadSauce()
+                .AddSecredIngredient()
+                .GetResult();
+        }
+    }
+
+}
